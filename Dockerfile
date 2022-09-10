5fb30c39cf61b8512611ad19238ea5f7be04f9e6
Jasper Chan <jasperchan515@gmail.com>
2022-09-10 03:20:27 -0700

    feature: install kicadStepUpMod to support KiCad exports

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM ghcr.io/gigahawk/freecad-cli:0.19.4_2
 
+RUN \
+    add-apt-repository --yes ppa:kicad/kicad-6.0-releases \
+    && apt update \
+    && apt install -y --install-recommends kicad
+
+RUN \
+    cd /usr/local/Mod/ \
+    && git clone https://github.com/easyw/kicadStepUpMod.git
+
 COPY entrypoint.sh /
 COPY export.py /
 
--- export.py
+++ export.py
@@ -9,6 +9,8 @@ import Import
 import ImportGui
 import Mesh
 import os
+from PySide import QtGui
+
 
 def get_export_func(output):
     suffix = output.suffix.lower()
@@ -24,6 +26,84 @@ def get_export_func(output):
     print("Using default exporter")
     return Import.export
 
+def set_step_export_prefs():
+    # Set step export prefs for kicadStepUp according to popup screenshot
+    # https://github.com/easyw/kicadStepUpMod/blob/master/demo/Import-Export-settings.png
+    print("Setting up STEP export prefs")
+
+    ### Export ###
+    # Set "Units for export of STEP" to "mm"
+    step_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Part/STEP")
+    step_prefs.SetInt("Unit", 0)
+
+    # Disable "Write out curves in parametric space of surface"
+    step_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Part/General")
+    step_prefs.SetInt("WriteSurfaceCurveMode", 0)
+
+    # Enable "Export invisible objects"
+    import_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Import")
+    import_prefs.SetBool("ExportHiddenObject", 1)
+
+    # Disable "Export single object placement"
+    import_prefs.SetBool("ExportKeepPlacement", 0)
+
+    # Disable "Use legacy exporter"
+    import_prefs.SetBool("ExportLegacy", 0)
+
+    # Set "Scheme" to "AP214 International Standard"
+    step_prefs.SetString("Scheme", "AP214IS")
+
+    ### Import ###
+
+    # Disable "Enable STEP Compound merge"
+    hstep_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Import/hSTEP")
+    hstep_prefs.SetBool("ReadShapeCompoundMode", 0)
+
+    # Disable "Use LinkGroup"
+    import_prefs.SetBool("UseLinkGroup", 0)
+
+    # Enable "Import invisible objects"
+    import_prefs.SetBool("ImportHiddenObject", 1)
+
+    # Disable "Reduce number of objects"
+    import_prefs.SetBool("ReduceObjects", 0)
+
+    # Enable "Expand compound shape"
+    import_prefs.SetBool("ExpandCompound", 1)
+
+    # Disable "Show progress bar when importing"
+    import_prefs.SetBool("ShowProgress", 0)
+
+    # Enable "Ignore instance names"
+    import_prefs.SetBool("UseBaseName", 1)
+
+    # Set "Mode" to "Single document"
+    import_prefs.SetInt("ImportMode", 0)
+
+
+def activate_kicad_wb():
+    print("Activating KiCadStepUp Workbench")
+    # Disable update checking (prevents blocking popups)
+    kicadstepup_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/kicadStepUp")
+    kicadstepup_prefs.SetBool("checkUpdates", 0)
+
+    # Disable help warning (prevents blocking popups)
+    import_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Import")
+    import_prefs.SetBool("help_warning_enabled", 0)
+
+    # Activate KiCadStepUp
+    FreeCADGui.activateWorkbench("KiCadStepUpWB")
+
+def fake_infobox(parent, title, text, *args, **kwargs):
+    print("info box shown")
+    print(f"parent: {parent}")
+    print(f"title: {title}")
+    print(f"text: {text}")
+    print(f"args: {args}")
+    print(f"kwargs: {kwargs}")
+
+QtGui.QMessageBox.information = fake_infobox
+
 
 def main(input_paths, output_path, export_types):
     output_path = Path(output_path)
@@ -31,9 +111,19 @@ def main(input_paths, output_path, export_types):
 
     for _p in input_paths:
         for p in glob(_p, recursive=True):
-            input = Path(p)
-            print(f"Opening {input}")
-            doc = FreeCAD.open(str(input))
+            input_path = Path(p)
+            print(f"Opening {input_path}")
+            if input_path.suffix.lower() == ".fcstd":
+                print(f"Standard FreeCAD project detected")
+                doc = FreeCAD.open(str(input_path))
+            elif input_path.suffix.lower() == ".kicad_pcb":
+                print(f"KiCad PCB file detected")
+                doc = FreeCAD.newDocument(input_path.name)
+                activate_kicad_wb()
+                import kicadStepUptools
+                kicadStepUptools.onLoadBoard(
+                    file_name=str(input_path),
+                    insert=True)
 
             print(f"Looking for root level objects")
             root_objs = [o for o in doc.Objects if not o.Parents]
@@ -41,7 +131,7 @@ def main(input_paths, output_path, export_types):
                 print(o.Label)
 
             for e in export_types:
-                output = output_path / input.with_suffix(f".{e}")
+                output = output_path / input_path.with_suffix(f".{e}")
                 output.parent.mkdir(parents=True, exist_ok=True)
                 print(f"Exporting to {output}")
                 export_func = get_export_func(output)
